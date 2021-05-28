577bc2a2c6067ade29d9c2966dd817338eaedc50
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-05-29 00:38:39 +0800

    removed bcrypto from mix.exs file

--- elixir_backend/mix.exs
+++ elixir_backend/mix.exs
@@ -51,7 +51,6 @@ defmodule ElixirBackend.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.0"},
       {:ueberauth, "~> 0.6"},
       {:ueberauth_discord, "~> 0.6"},
       {:ueberauth_github, "~> 0.7"}
