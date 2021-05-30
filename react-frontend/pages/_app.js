db4636eb002b18f273b3dbc8faa30fe689c19a99
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-05-31 03:19:42 +0800

    done for milestone1

--- react-frontend/pages/_app.js
+++ react-frontend/pages/_app.js
@@ -1,13 +1,17 @@
 import '@fontsource/inter'
 import { ChakraProvider } from '@chakra-ui/react'
+
 import customTheme from '../styles/theme'
+import { AuthProvider } from '../context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChakraProvider resetCSS theme={customTheme} >
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider resetCSS theme={customTheme} >
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider> 
   )
 }
 
