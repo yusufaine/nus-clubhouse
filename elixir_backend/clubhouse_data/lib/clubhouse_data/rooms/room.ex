98a8dfcda89b9194e62cb7b2e5b193834f3be4d1
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2021-07-01 11:51:21 +0800

    Revert "testing branch"
    This reverts commit 43d19eaaac08aaafb8d3a70025a7873e667cb0b7.

--- elixir_backend/clubhouse_data/lib/clubhouse_data/rooms/room.ex
+++ elixir_backend/clubhouse_data/lib/clubhouse_data/rooms/room.ex
@@ -7,8 +7,7 @@ defmodule ClubhouseData.Rooms.Room do
         field :name, :string
         field :numUsers, :integer
         field :type, :string
-        field :isLive, :boolean
-        belongs_to :creator, ClubhouseData.Users.User
+        belongs_to :creator, ClubhouseData.Users.User 
         many_to_many :users, ClubhouseData.Users.User, join_through: "rooms_users"
 
         timestamps()
