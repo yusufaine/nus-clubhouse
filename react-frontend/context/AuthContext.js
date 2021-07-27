63fd7790cd151c7fda1bbbd8437c0a76a8fc8dac
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-07-28 06:56:23 +0800

    updated fetchScheduled api url

--- react-frontend/context/AuthContext.js
+++ react-frontend/context/AuthContext.js
@@ -60,7 +60,7 @@ export const AuthProvider = (props) => {
 
     const fetchScheduledRooms = async () => {
         try {
-            const data = await callAPI('/rooms?isScheduled=true', 'GET')
+            const data = await callAPI('/rooms/scheduled', 'GET')
             if(!data.data) {
                 console.error("Failed to load scheduled rooms. Please try again. ", data)
             } else {
